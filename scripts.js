c054363e589bba830d2deb442a4751a799565a2f
aplum004 <143533266+aplum004@users.noreply.github.com>
2025-11-10 20:43:48 -0500

    Implement sidebar toggle functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+//Sidebar toggle
+var sidebarOpen = False
+var sidebar = document.getElementById("sidebar");
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
 
+function closesSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
