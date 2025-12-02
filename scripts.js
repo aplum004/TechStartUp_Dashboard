7e559c4ebc44ce3c9dd7d137552bef7bc52ceea3
aplum004 <143533266+aplum004@users.noreply.github.com>
2025-12-01 21:09:29 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,60 +1,55 @@
-//Sidebar toggle
+// Sidebar toggle
 let sidebarOpen = false;
 const sidebar = document.getElementById("sidebar");
+
 function openSidebar() {
-  if(!sidebarOpen) {
+  if (!sidebarOpen) {
     sidebar.classList.add("sidebar-responsive");
     sidebarOpen = true;
   }
 }
 
-function closesSidebar() {
-  if(sidebarOpen) {
+function closeSidebar() {
+  if (sidebarOpen) {
     sidebar.classList.remove("sidebar-responsive");
     sidebarOpen = false;
   }
 }
-//bar chart 
+
+// Bar Chart
 var barChartOptions = {
   series: [{
     data: [10, 8, 6, 4, 2]
   }],
+
   chart: {
     type: 'bar',
-    height: 358,
-    toolbar: {
-      show: false
-    },
+    height: 380,
+    toolbar: { show: false }
   },
-  colors: [
-    "#246dec",
-    "#cc3c43",
-    "#367952",
-    "#f5b74f",
-    "#4f35a1"
-  ],
+
+  colors: ["#246dec", "#cc3c43", "#367952", "#f5b74f", "#4f35a1"],
+
   plotOptions: {
     bar: {
       distributed: true,
       borderRadius: 4,
-      horizontal: false,
       columnWidth: '40%',
     }
   },
-  dataLabels: {
-    enabled: false
-  },
-  legend: {
-    show: false
-  },
+
+  dataLabels: { enabled: false },
+  legend: { show: false },
+
   xaxis: {
-  categories:['Lucid Motors', 'Tesla', 'Waymo', 'Hyundai Ioniq', ' Mercedes S-Class EQS', 'China','Germany'],
-    },
-yaxis: {
-  title: {
-    text: "Count"
+    categories: ['Lucid Motors', 'Tesla', 'Waymo', 'Hyundai Ioniq', 'Mercedes EQS']
+  },
+
+  yaxis: {
+    title: { text: "Count" }
   }
 };
+
 var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
 barChart.render();
 
