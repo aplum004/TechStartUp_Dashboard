e6b7da34285dff48a835521af20f4d74a9c32c23
aplum004 <143533266+aplum004@users.noreply.github.com>
2025-12-01 20:38:46 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,3 +14,50 @@ function closesSidebar() {
     sidebarOpen = false;
   }
 }
+//bar chart 
+var barChartOptions = {
+  series: [{
+    data: [10, 8, 6, 4, 2]
+  }],
+  chart: {
+    type: 'bar',
+    height: 358,
+    toolbar: {
+      show: false
+    },
+  },
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f5b74f",
+    "#4f35a1"
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+  categories:['Lucid Motors', 'Tesla', 'Waymo', 'Hyundai Ioniq', ' Mercedes S-Class EQS', 'China','Germany'],
+    },
+yaxis: {
+  title: {
+    text: "Count"
+  }
+};
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+    
+
+  
